ea03e795c91fb58c8110ee6e384c2a641e124478
enkidulan <supamaxy@gmail.com>
2014-01-24 13:02:32 +0200

    progress status - replace modal dialog with page; separate views into templates

--- slidelint_site/static/js/app.js
+++ slidelint_site/static/js/app.js
@@ -1,41 +1,34 @@
-LinterApp = angular.module('LinerApp', ['angularFileUpload', 'ui.bootstrap']);
-LinterApp.factory('GlobalDataSharing', function() {
-  var upload_file_info = {
-      uploading_progress : 0
-  }
-  return {
-    upload_file_info : upload_file_info
-  };
-});
+LinterApp = angular.module('LinterApp', ['angularFileUpload', 'ui.bootstrap']);
 
-var LinterCtrl = [ '$scope', 'GlobalDataSharing', '$upload', '$http', '$timeout', '$modal',
-  function($scope, GlobalDataSharing, $upload, $http, $timeout, $modal) {
+var LinterCtrl = ['$scope', '$upload', '$http', '$timeout', '$modal',
+  function($scope, $upload, $http, $timeout, $modal) {
+  $scope.state = 'upload';
   $scope.check_rule = 'simple';
   $scope.job_uid = false;
   $scope.error = false;
   $scope.results = [];
-  $scope.upload_file_info = GlobalDataSharing.upload_file_info;
+  $scope.show_upload = function(){
+    $scope.state = 'upload';
+  }
   $scope.getResults = function(){
     if ($scope.job_uid !== false){
       $http.post('/results', {'uid': $scope.job_uid},
           {headers: {'Content-Type': 'application/json'}})
         .success(function(data, status, headers, config) {
             $scope.job_uid = false;
-            $scope.upload_file_info.uploading_progress = 0;
+            $scope.uploading_progress = 0;
             $scope.icons = data.icons;
             $scope.results = data.result;
-            $scope.showResults = true;
-            $scope.modalInstance.dismiss();
+            $scope.state = 'results';
         }).
         error(function(data, status, headers, config) {
           // called asynchronously if an error occurs
           // or server returns response with an error status.
           if (status === 500) {
               $scope.job_uid = false;
-              $scope.upload_file_info.uploading_progress = 0;
+              $scope.uploading_progress = 0;
               $scope.error = data.result;
-              $scope.showResults = true;
-              $scope.modalInstance.dismiss();
+              $scope.state = 'results';
           };
         });
     };
@@ -44,14 +37,14 @@ var LinterCtrl = [ '$scope', 'GlobalDataSharing', '$upload', '$http', '$timeout'
     $timeout(function() {
       $scope.getResults();
       $scope.intervalFunction();
-    }, 1000)
+    }, 3000)
   };
   $scope.intervalFunction();
   $scope.onFileSelect = function($files) {
     //$files: an array of files selected, each file has name, size, and type.
     for (var i = 0; i < $files.length; i++) {
       var file = $files[i];
-      $scope.open_waiting_modal()
+      $scope.state = 'progress';
       $scope.upload = $upload.upload({
         url: '/upload',
         // method: POST or PUT,
@@ -64,7 +57,7 @@ var LinterCtrl = [ '$scope', 'GlobalDataSharing', '$upload', '$http', '$timeout'
         /* customize how data is added to formData. See #40#issuecomment-28612000 for example */
         //formDataAppender: function(formData, key, val){}
       }).progress(function(evt) {
-        $scope.upload_file_info.uploading_progress = parseInt(100.0 * evt.loaded / evt.total);
+        $scope.uploading_progress = parseInt(100.0 * evt.loaded / evt.total);
       }).success(function(data, status, headers, config) {
         // file is uploaded successfully
         // data = JSON.parse(data);
@@ -78,36 +71,4 @@ var LinterCtrl = [ '$scope', 'GlobalDataSharing', '$upload', '$http', '$timeout'
     }
   };
 
-  $scope.open_waiting_modal = function () {
-
-    $scope.modalInstance = $modal.open({
-        templateUrl: '/static/templates/waiting.html',
-        controller: ModalInstanceCtrl,
-        resolve: {}
-    });
-
-    // $scope.modalInstance.result.then(function (selectedItem) {
-    //   $scope.selected = selectedItem;
-    // }, function () {
-    //   console.log('Modal dismissed at: ' + new Date());
-    // });
-  };
-
 }];
-
-var ModalInstanceCtrl = function ($scope, GlobalDataSharing, $modalInstance) {
-  $scope.upload_file_info = GlobalDataSharing.upload_file_info;
-  // $scope.uploading_complete = $scope.upload_file_info.uploading_progress==100 && true || false;
-  // $scope.processing_image =  function () {
-  //   type =
-  //   url = '/static/img/wait-' + type + '.gif';
-  //   return url
-  // }
-  // $scope.ok = function () {
-  //   $modalInstance.close($scope.selected.item);
-  // };
-
-  // $scope.cancel = function () {
-  //   $modalInstance.dismiss('cancel');
-  // };
-};
