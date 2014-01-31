b53e678002c2dea40c9d0353e77f6040555579de
enkidulan <supamaxy@gmail.com>
2014-02-01 00:44:01 +0200

    added feedback view

--- slidelint_site/views.py
+++ slidelint_site/views.py
@@ -5,9 +5,38 @@ from pyramid.view import view_config
 from pyramid.renderers import render
 from pyramid.response import Response
 from .validators import validate_rule, validate_upload_file
+from pyramid_mailer.message import Message
+from pyramid_mailer import get_mailer
+import transaction
 
 import logging
 LOGGER = logging.getLogger(__name__)
+
+
+@view_config(route_name='feedback', request_method="POST", renderer='json')
+def feedback(request):
+    """
+    Feedback view - send to site administrator user feedback message
+    """
+    mgs = request.json_body.get('message', None)
+    uid = request.json_body.get('uid', '')
+    if not mgs or uid:
+        request.response.status_code = 400
+        return {'error': 'you should provide message and job uid'}
+
+    mailer = get_mailer(request)
+    settings = request.registry.settings
+
+    body = "Job id: %s\nFeedback text:\n%s" % (uid, mgs)
+
+    message = Message(
+        subject=settings['mail.subject'],
+        sender=settings['mail.sender'],
+        recipients=settings['mail.recipients'],
+        body=body)
+    mailer.send(message)
+    transaction.commit()
+    return {'status': 'ok'}
 
 
 @view_config(context='.models.Counter')
